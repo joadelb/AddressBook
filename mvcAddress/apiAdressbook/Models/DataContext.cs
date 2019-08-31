5ce947d8ccd09071a71724cb25f5e619e92c0109
Joadel Bravo <joadelb@gmail.com>
2019-08-31 09:52:26 -0400

    web api ok

---
+++ mvcAddress/apiAdressbook/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace apiAdressbook.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiAdressbook.Models.Book> Books { get; set; }
+    }
+}
