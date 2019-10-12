3359501e250c6b4bd44021d713e81f8823c1f788
faltamirano98@hotmail.com <faltamirano98@hotmail.com>
2019-10-12 11:05:25 -0400

    Terminado

---
+++ mvcAddress/appAddress/appAddress/VIews/bookpage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appAddress.VIews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class bookpage : ContentPage
+    {
+
+        public bookpage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
